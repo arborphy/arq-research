8c25b228aa6b6ebb2aed7b7febae87735c794ed9
Austin Garrard <austin.garrard@relational.ai>
2025-11-10 11:04:43 -0500

    reuse model between tests

--- kg/tests/conftest.py
+++ kg/tests/conftest.py
@@ -4,6 +4,6 @@ import relationalai.semantics as rai
 from kg.model import define_arq, ARQModel
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def arq() -> ARQModel:
     return define_arq(rai.Model(f"arq_test"))
